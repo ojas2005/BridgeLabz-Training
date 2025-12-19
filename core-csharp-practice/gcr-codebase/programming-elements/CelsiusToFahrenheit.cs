b534c03a729b47f3e872a16495c221c94767bce1
ojas2005 <115890213+ojas2005@users.noreply.github.com>
2025-12-19 10:25:00 +0530

    Delete core-csharp-practice/gcr-codebase/programming-elements directory

--- core-csharp-practice/gcr-codebase/programming-elements/CelsiusToFahrenheit.cs
+++
@@ -1,9 +0,0 @@
-using System;
-class CelsiusToFahrenheit{
-    static void Main()
-    {
-        float cels = float.Parse(Console.ReadLine()); //Taking the celsius value as input
-        float fahr = (cels * 1.8f)+32f; //Converting the celsius value in fahrenheit.
-        Console.WriteLine(fahr); //Printing results.
-    }
-}
--- core-csharp-practice/gcr-codebase/programming-elements/WelcomeToBridgeLabz.cs
+++
@@ -1,7 +0,0 @@
-using System;
-class WelcomeToBridgeLabz{
-    static void Main()
-    {
-        Console.WriteLine("Welcome to BridgeLabz"); //Printing Welcome to BridgeLabz
-    }
-}
