c547272a051295d4d8db6dadea665a8d8af131e8
Ojas Tiwari <ojastiwari@Ojass-MacBook-Air.local>
2026-02-07 15:27:04 +0530

    [ojas] uc1.2

--- dbms-csharp-practice/scenario-based/HealthClinicApp.sql
+++ dbms-csharp-practice/scenario-based/HealthClinicApp.sql
@@ -3,4 +3,5 @@ GO
 USE HealthClinicDB;
 
 CREATE TABLE Patients(patientId INT IDENTITY(1,1) PRIMARY KEY,name NVARCHAR(100) NOT NULL,dob DATE,phone NVARCHAR(15) UNIQUE,email NVARCHAR(100) UNIQUE,address NVARCHAR(255),blood_group NVARCHAR(5));
-INSERT INTO Patients (name,dob,phone,email,address,blood_group) VALUES ('Rahul Sharma','1998-05-10','9897851033','rahul@gmail.com','Delhi','O+');
+INSERT INTO Patients(name,dob,phone,email,address,blood_group) VALUES ('Rahul Sharma','1998-05-10','9897851033','rahul@gmail.com','Delhi','O+');
+UPDATE Patients SET address = 'Goa',blood_group = 'A+' WHERE patientId = 1;
